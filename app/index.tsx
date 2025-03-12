d0a3f9e5a58c7558f140a8bef88fd117c1afc47e
hun <pursuit0819@naver.com>
2025-03-12 21:25:59 +0900

    scroll view

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import {
   Pressable,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
@@ -11,6 +12,10 @@ import {
 import { StatusBar } from 'expo-status-bar';
 import { theme } from '@/color';
 import { useState } from 'react';
+
+type toDosType = {
+  [key: string]: any;
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -29,19 +34,41 @@ const styles = StyleSheet.create({
     color: theme.grey,
   },
   input: {
-    marginTop: 10,
+    marginTop: 20,
     fontSize: 18,
     backgroundColor: 'white',
     paddingVertical: 15,
     paddingHorizontal: 15,
     borderRadius: 40,
   },
+  toDos: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 15,
+    backgroundColor: theme.toDoBg,
+  },
+  toDosText: {
+    fontSize: 20,
+    color: 'white',
+  },
 });
 
 export default function Index() {
-  const [working, setWorking] = useState(false);
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState<string>('');
+  const [toDos, setToDos] = useState<toDosType>({});
+
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
+
+  const onChangeText = (payload: string) => setText(payload);
+
+  const alertMsg = () => {
+    if (text === '') return;
+    setToDos((prev) => ({ ...prev, [Date.now()]: { text, work: working } }));
+    setText('');
+  };
 
   return (
     <View style={styles.container}>
@@ -49,24 +76,24 @@ export default function Index() {
 
       <View style={styles.header}>
         <Pressable
-          onPress={travel}
-          hitSlop={30}
-          pressRetentionOffset={{ bottom: 20, left: 20, right: 20, top: 20 }}
+          onPress={work}
+          hitSlop={10}
+          pressRetentionOffset={{ bottom: 10, left: 10, right: 10, top: 10 }}
         >
           <Text
-            style={{ ...styles.button, color: !working ? 'white' : theme.grey }}
+            style={{ ...styles.button, color: working ? 'white' : theme.grey }}
           >
             Work
           </Text>
         </Pressable>
 
         <Pressable
-          onPress={work}
-          hitSlop={30}
-          pressRetentionOffset={{ bottom: 20, left: 20, right: 20, top: 20 }}
+          onPress={travel}
+          hitSlop={10}
+          pressRetentionOffset={{ bottom: 10, left: 10, right: 10, top: 10 }}
         >
           <Text
-            style={{ ...styles.button, color: working ? 'white' : theme.grey }}
+            style={{ ...styles.button, color: !working ? 'white' : theme.grey }}
           >
             Travel
           </Text>
@@ -74,10 +101,23 @@ export default function Index() {
       </View>
 
       <TextInput
+        onSubmitEditing={alertMsg}
         style={styles.input}
-        placeholder={!working ? 'Add To do!' : 'Where do you want to go?'}
+        autoCapitalize={'sentences'}
+        onChangeText={onChangeText}
+        value={text}
+        returnKeyType={working ? 'done' : 'send'}
+        placeholder={working ? 'Add To do!' : 'Where do you want to go?'}
         placeholderTextColor={'#ddd'}
-      ></TextInput>
+      />
+
+      <ScrollView style={styles.main}>
+        {Object.keys(toDos).map((key, index) => (
+          <View style={styles.toDos} key={index}>
+            <Text style={styles.toDosText}>{toDos[key].text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
